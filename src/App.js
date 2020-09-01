4e7d93fcb1266c50be5a7db995128b5bb6b82bff
Hani Husamuddin <hani.husam@gmail.com>
2020-09-01 13:53:04 +0700

    init start screen to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import StartScreen from "./components/StartScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">Main App</header>
-    </div>
+    <main className="h-app">
+      <StartScreen />
+    </main>
   );
 }
 
