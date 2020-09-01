1656131bcea2275d9b3679710d0e35dbd4affc36
Hani Husamuddin <hani.husam@gmail.com>
2020-09-01 22:48:29 +0700

    resetup name of component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Main from "./components/Main";
+import MainScreen from "./components/MainScreen";
 
 const App = () => {
   return (
     <Router>
-      <Route exact path="/" component={Main} />
+      <Route exact path="/" component={MainScreen} />
     </Router>
   );
 };
