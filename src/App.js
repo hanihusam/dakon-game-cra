aaaab4b766f61c1dd3443f17a38b218c1fe4b68d
Hani Husamuddin <hani.husam@gmail.com>
2020-09-01 20:07:59 +0700

    setup main route

--- src/App.js
+++ src/App.js
@@ -1,34 +1,13 @@
-import React, { useState } from "react";
-import StartScreen from "./components/StartScreen";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Main from "./components/Main";
 
 const App = () => {
-  const [tipeSoal, setTipeSoal] = useState("");
-  const [bilangan, setBilangan] = useState([]);
-
-  const getType = value => setTipeSoal(value);
-
-  const getBilangan = value => {
-    const position = bilangan.indexOf(value);
-    let newBilangan = bilangan.slice();
-
-    if (position !== -1) {
-      newBilangan.splice(position, 1);
-    } else {
-      newBilangan = [...bilangan, value];
-    }
-
-    setBilangan(newBilangan);
-  };
-
   return (
-    <main className="h-app">
-      <StartScreen
-        getBilangan={getBilangan}
-        getType={getType}
-        selectedType={tipeSoal}
-        selectedBilangan={bilangan}
-      />
-    </main>
+    <Router>
+      <Route exact path="/" component={Main} />
+    </Router>
   );
 };
 
