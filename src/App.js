98dfec2743d0b60313463af4fbbae1918a63fabf
Hani Husamuddin <hani.husam@gmail.com>
2020-09-01 19:40:29 +0700

    init getBilangan fn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import StartScreen from "./components/StartScreen";
 
-function App() {
+const App = () => {
+  const [tipeSoal, setTipeSoal] = useState("");
+  const [bilangan, setBilangan] = useState([]);
+
+  const getType = value => setTipeSoal(value);
+
+  const getBilangan = value => {
+    const position = bilangan.indexOf(value);
+    let newBilangan = bilangan.slice();
+
+    if (position !== -1) {
+      newBilangan.splice(position, 1);
+    } else {
+      newBilangan = [...bilangan, value];
+    }
+
+    setBilangan(newBilangan);
+  };
+
   return (
     <main className="h-app">
-      <StartScreen />
+      <StartScreen
+        getBilangan={getBilangan}
+        getType={getType}
+        selectedType={tipeSoal}
+        selectedBilangan={bilangan}
+      />
     </main>
   );
-}
+};
 
 export default App;
