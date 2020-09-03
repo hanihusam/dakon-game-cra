bca074caaad8a5513b5568032f7caf9267307136
Hani Husamuddin <hani.husam@gmail.com>
2020-09-03 22:25:16 +0700

    set get kelipatan fn to the component

--- src/components/GameBoard/NumberBoard.js
+++ src/components/GameBoard/NumberBoard.js
@@ -4,26 +4,36 @@ import styled from "styled-components";
 import { Button } from "react-bootstrap";
 
 const NumberBoard = props => {
-  const { angka } = props;
+  const { angka, getKelipatanPertama, getKelipatanKedua } = props;
 
   return (
     <div className="number-board">
       <Board>{angka}</Board>
       <div className="sub">
-        <i className="far fa-star"></i>
-        <i className="far fa-circle"></i>
+        <i
+          className="far fa-star"
+          onClick={() => getKelipatanPertama(angka)}
+        ></i>
+        <i
+          className="far fa-circle"
+          onClick={() => getKelipatanKedua(angka)}
+        ></i>
       </div>
     </div>
   );
 };
 
 const Board = styled(Button)`
-  width: 64px;
-  height: 64px;
+  width: 4em;
+  height: 4em;
   border-radius: 10px;
   border-color: #87431d !important;
-  color: #87431d;
-  background-color: transparent;
+  color: #dbcbbd;
+  background-color: #87431d;
+  &:hover {
+    color: #dbcbbd;
+    background-color: #290001;
+  }
   &:focus {
     outline: 0;
     color: #fff;
@@ -41,7 +51,9 @@ const Board = styled(Button)`
 `;
 
 NumberBoard.propTypes = {
-  angka: PropTypes.number
+  angka: PropTypes.number,
+  getKelipatanPertama: PropTypes.func,
+  getKelipatanKedua: PropTypes.func
 };
 
 export default NumberBoard;
