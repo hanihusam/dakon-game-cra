4c8f98b51ec95dc9373a27d4e945f68bb222c715
Hani Husamuddin <hani.husam@gmail.com>
2020-09-06 18:21:14 +0700

    update setup server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
-const express = require("express");
-const app = express();
-
 const path = require("path");
 const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, "..", "public");
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "build", "index.html"))
+  );
+}
+
 const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
-});
 app.listen(port, () => {
   console.log("Server is up!");
 });
