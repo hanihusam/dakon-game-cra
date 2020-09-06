9aedf2508afcef086fe0ccce7add17755cca3dc6
Hani Husamuddin <hani.husam@gmail.com>
2020-09-06 15:03:31 +0700

    create the result dialog

---
+++ src/components/ResultDialog.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Modal, Button, Card } from "react-bootstrap";
+
+import WinImg from "../img/win.svg";
+import LoseImg from "../img/lose.svg";
+
+const ResultDialog = ({ win, show, handleClose }) => {
+  const onRefreshPage = () => {
+    window.location.reload(false);
+  };
+
+  return (
+    <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+      <Modal.Body>
+        <Card.Img variant="top" src={win ? WinImg : LoseImg} className="my-3" />
+        <Card.Body className="text-center">
+          <h1 className="text-uppercase">{win ? "BENAR" : "SALAH"}</h1>
+        </Card.Body>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="primary" block onClick={onRefreshPage}>
+          Mulai Main Lagi
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+ResultDialog.propTypes = {
+  won: PropTypes.bool,
+  show: PropTypes.bool.isRequired,
+  handleClose: PropTypes.func.isRequired
+};
+
+export default ResultDialog;
